5ac29e6ffc1d7fda1c34fe251aad442ec00faa21
roo2bos <roo2bos@gmail.com>
2024-03-06 13:59:41 +0900

    메인 ui 추가 / react-icons 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
+import NotFound from './components/NotFound';
+import Guide from './components/Guide';
 import Home from './pages/Home';
 import Mypage from './pages/Mypage';
-import Guide from './components/Guide';
-import NotFound from './components/NotFound';
+import Talk from './pages/Talk'; // Talk
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route element={<Layout />}>
 					<Route path="/" element={<Home />} />
+					<Route path="/talk" element={<Talk />} />
 					<Route path="/mypage" element={<Mypage />} />
 					<Route path="/guide" element={<Guide />} />
 					<Route path="*" element={<NotFound />} />
