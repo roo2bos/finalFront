0872eaa8f5242a935af07796a3db5fe22d7442ef
roo2bos <roo2bos@gmail.com>
2024-03-07 13:59:29 +0900

    레이아웃 머지

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,21 +3,11 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import Lnb from './components/Lnb';
 
 export default function Layout() {
-  return (
-    <div id='wrap'>
-      <Nav />
-      <main>
-        <Header />
-        <Outlet />
-      </main>
-    </div>
-  );
 	return (
 		<div id="wrap">
-			<Lnb />
+			<Nav />
 			<main>
 				<Header />
 				<div className="content">
