c373b083508b9ef9b7a6fca1622d38c8a91a5237
roo2bos <roo2bos@gmail.com>
2024-03-08 11:02:53 +0900

    레이아웃 파일 중복 수정

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
 
 export default function Layout() {
 	return (
