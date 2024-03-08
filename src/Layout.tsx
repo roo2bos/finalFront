c36d7ec4327723ea21ca43d7a78094c019b040d2
jihyehan91 <toto5523@hanmail.net>
2024-03-08 12:33:45 +0900

    nav 수정

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,20 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
 
 export default function Layout() {
-	return (
-		<div id="wrap">
-			<Nav />
-			<main>
-				<Header />
-				<div className="content">
-					<Outlet />
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div id='wrap'>
+      <Nav />
+      <main>
+        <Header />
+        <div className='content'>
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
 }
-
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,27 @@
-import '../assets/nav.css';
+import '../assets/css/nav.css';
+import { Link } from 'react-router-dom';
 
 export default function Lnb() {
   return (
-    <div id='nav'>
-      <div className='nav'>
-        <h1>LOGO</h1>
-        <div className='nav-user'></div>
-        <ul className='nav-menu-list'>
-          <li className='nav-menu-item'>메뉴1</li>
-          <li className='nav-menu-item'>메뉴2</li>
-          <li className='nav-menu-item'>메뉴3</li>
-        </ul>
-        <div>@copyrightm</div>
+    <nav id='nav'>
+      <div className='nav-container'>
+        <div className='nav-content'>
+          <h1 className='logo'>DoRun-DoRun</h1>
+          <div className='nav-user'>
+            <div className='nav-user-login'>로그인</div>
+          </div>
+          <ul className='nav-menu-list'>
+            <li className='nav-menu-item'>
+              <Link to='/'>홈</Link>
+            </li>
+            <li className='nav-menu-item'>메뉴2</li>
+            <li className='nav-menu-item'>메뉴3</li>
+          </ul>
+        </div>
+        <div className='nav-footer'>
+          <p>@copyright</p>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
