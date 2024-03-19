bb8d883be5ea55c4c924955776c720e31ad3235b
ghtjd1358 <hojjang18@naver.com>
2024-03-19 21:11:10 +0900

    페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Layout from './Layout';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
@@ -10,25 +11,29 @@ import Talk from './pages/Talk';
 import Myroom from './pages/Myroom';
 import Character from './pages/Character';
 import Wordquiz from './components/Wordquiz';
+import Spinner from './components/Spinner';
 import './App.css';
 
 function App() {
+
   return (
     <>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/talk/:id' element={<Talk />} />
-          <Route path='/mypage' element={<Mypage />} />
-          <Route path='/myroom' element={<Myroom />} />
-          <Route path='/guide' element={<Guide />} />
-          <Route path='/character/:id' element={<Character />} />
-          <Route path='/quiz' element={<Wordquiz />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/talk/:id' element={<Talk />} />
+            <Route path='/mypage' element={<Mypage />} />
+            <Route path='/myroom' element={<Myroom />} />
+            <Route path='/guide' element={<Guide />} />
+            <Route path='/character/:id' element={<Character />} />
+            <Route path='/quiz' element={<Wordquiz />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/spinner' element={<Spinner />} />
+        </Routes>
+     
     </>
   );
 }
